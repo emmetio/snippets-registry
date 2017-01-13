b3d848a51bd6818b481d8395be4b08bc9f562414
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-13 11:46:24 +0300

    👷🏻 Updated builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
 export default {
-    exports: 'named'
+	entry: './index.js',
+    exports: 'named',
+	targets: [
+		{format: 'cjs', dest: 'dist/snippets-registry.cjs.js'},
+		{format: 'es',  dest: 'dist/snippets-registry.es.js'}
+	]
 };
