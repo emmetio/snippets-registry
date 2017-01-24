5d7c0582148f9d27054494a117cb3319849131af
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-25 02:08:50 +0300

    Removed named export in Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 export default {
 	entry: './index.js',
-    exports: 'named',
 	targets: [
 		{format: 'cjs', dest: 'dist/snippets-registry.cjs.js'},
 		{format: 'es',  dest: 'dist/snippets-registry.es.js'}
